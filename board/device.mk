240e5494ad3a8c04cf8b7bf76326272c6156aff0
PHOENIX2696 <pprag2821990@gmail.com>
2018-01-09 10:35:00 +0530

    Initial Commit

---
+++ board/device.mk
@@ -0,0 +1,10 @@
+# OTA assert
+TARGET_OTA_ASSERT_DEVICE := marino_f,XT1902-2,LENOVO K8 PLUS
+
+# Bionic
+
+# Enable Minikin text layout engine (will be the default soon)
+USE_MINIKIN := true
+
+# Configure jemalloc for low memory
+MALLOC_SVELTE := true
---
+++ board/telephony.mk
@@ -0,0 +1,3 @@
+# RIL
+BOARD_RIL_CLASS := ../../../device/lenovo/marino_f/ril
+BOARD_CONNECTIVITY_MODULE := conn_soc
